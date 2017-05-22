3ce385cff55003ba37f3fc353ff67b507bdac6ab
Frank Iriarte <jurrish@gmail.com>
2017-05-22 10:27:50 -0700

    boops

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 'use strict';
 
+angular.module('demoApp', [])
+//has more than one argument so it's a setter
+.component('yodaSpeak', {
+  template: `<div>
+    <p> booya this is a yoda-speak component </p>
+  </div>
+  `,
+});
+
 console.log('hello world');
